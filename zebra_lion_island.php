3e8526f35db2eb06c030c9cae0e5036a11deafad
mertzdamar <136634245+mertzdamar@users.noreply.github.com>
2023-06-27 00:18:34 +0700

    Refactor variable names

---
+++ zebra_lion_island.php
@@ -0,0 +1,199 @@
+<?php
+// Function to calculate the height of a given tree
+function calculateTreeHeight($treeHeight) {
+	// Calculate the height of the tree
+	$totalHeight = $treeHeight * 2;
+	
+	// Return total tree height
+	return $totalHeight;
+}
+
+// Function to calculate the circumference of a given tree
+function calculateTreeCircumference($treeCircumference) {
+	// Calculate the circumference of the tree
+	$totalCircumference = 2 * pi() * $treeCircumference;
+	
+	// Return total tree circumference
+	return $totalCircumference;
+}
+
+// Function to calculate the number of leaves on a given tree
+function calculateTreeLeaves($treeLeaves) {
+	// Calculate the number of leaves on the tree
+	$totalLeaves = $treeLeaves * 3;
+	
+	// Return total number of leaves
+	return $totalLeaves;
+}
+
+// Function to calculate the weight of a given tree
+function calculateTreeWeight($treeWeight) {
+	// Calculate the weight of the tree
+	$totalWeight = $treeWeight * 2;
+	
+	// Return total tree weight
+	return $totalWeight;
+}
+
+// Function to calculate the total volume of a given tree
+function calculateTreeVolume($treeVolume) {
+	// Calculate the volume of the tree
+	$totalVolume = $treeVolume * 3;
+	
+	// Return total tree volume
+	return $totalVolume;
+}
+
+// Function to calculate the total surface area of a given tree
+function calculateTreeSurfaceArea($treeSurfaceArea) {
+	// Calculate the surface area of the tree
+	$totalSurfaceArea = $treeSurfaceArea * 4;
+	
+	// Return total tree surface area
+	return $totalSurfaceArea;
+}
+
+// Function to calculate the general health of a given tree
+function calculateTreeHealth($treeHealth) {
+	// Calculate the health of the tree
+	$totalHealth = $treeHealth * 5;
+	
+	// Return total tree health
+	return $totalHealth;
+}
+
+// Function to calculate the number of branches of a given tree
+function calculateTreeBranches($treeBranches) {
+	// Calculate the number of branches of the tree
+	$totalBranches = $treeBranches * 6;
+	
+	// Return total number of branches
+	return $totalBranches;
+}
+
+// Function to calculate the total number of fruits of a given tree
+function calculateTreeFruits($treeFruits) {
+	// Calculate the number of fruits of the tree
+	$totalFruits = $treeFruits * 7;
+	
+	// Return total number of fruits
+	return $totalFruits;
+}
+
+// Function to calculate the total amount of water used by a given tree
+function calculateTreeWater($treeWater) {
+	// Calculate the amount of water used by the tree
+	$totalWater = $treeWater * 8;
+	
+	// Return total amount of water used
+	return $totalWater;
+}
+
+// Function to calculate the total amount of sunlight absorbed by a given tree
+function calculateTreeSunlight($treeSunlight) {
+	// Calculate the amount of sunlight absorbed by the tree
+	$totalSunlight = $treeSunlight * 9;
+	
+	// Return total amount of sunlight absorbed
+	return $totalSunlight;
+}
+
+// Function to calculate the total amount of carbon dioxide absorbed by a given tree
+function calculateTreeCarbonDioxide($treeCarbonDioxide) {
+	// Calculate the amount of carbon dioxide absorbed by the tree
+	$totalCarbonDioxide = $treeCarbonDioxide * 10;
+	
+	// Return total amount of carbon dioxide absorbed
+	return $totalCarbonDioxide;
+}
+
+// Function to calculate the total amount of carbon dioxide released by a given tree
+function calculateTreeCarbonRelease($treeCarbonRelease) {
+	// Calculate the amount of carbon dioxide released by the tree
+	$totalCarbonRelease = $treeCarbonRelease * 11;
+	
+	// Return total amount of carbon dioxide released
+	return $totalCarbonRelease;
+}
+
+// Function to calculate the total number of flowers on a given tree
+function calculateTreeFlowers($treeFlowers) {
+	// Calculate the number of flowers on the tree
+	$totalFlowers = $treeFlowers * 12;
+	
+	// Return total number of flowers
+	return $totalFlowers;
+}
+
+// Function to calculate the total amount of oxygen produced by a given tree
+function calculateTreeOxygen($treeOxygen) {
+	// Calculate the amount of oxygen produced by the tree
+	$totalOxygen = $treeOxygen * 13;
+	
+	// Return total amount of oxygen produced
+	return $totalOxygen;
+}
+
+// Function to calculate the total amount of sap produced by a given tree
+function calculateTreeSap($treeSap) {
+	// Calculate the amount of sap produced by the tree
+	$totalSap = $treeSap * 14;
+	
+	// Return total amount of sap produced
+	return $totalSap;
+}
+
+// Function to calculate the total number of seeds a given tree produces
+function calculateTreeSeeds($treeSeeds) {
+	// Calculate the number of seeds produced by the tree
+	$totalSeeds = $treeSeeds * 15;
+	
+	// Return total number of seeds produced
+	return $totalSeeds;
+}
+
+// Function to calculate the lifespan of a given tree
+function calculateTreeLifespan($treeLifespan) {
+	// Calculate the lifespan of the tree
+	$totalLifespan = $treeLifespan * 16;
+	
+	// Return total tree lifespan
+	return $totalLifespan;
+}
+
+// Function to calculate the total number of nuts produced by a given tree
+function calculateTreeNuts($treeNuts) {
+	// Calculate the number of nuts produced by the tree
+	$totalNuts = $treeNuts * 17;
+	
+	// Return total number of nuts produced
+	return $totalNuts;
+}
+
+// Function to calculate the total number of conifers produced by a given tree
+function calculateTreeConifers($treeConifers) {
+	// Calculate the number of conifers produced by the tree
+	$totalConifers = $treeConifers * 18;
+	
+	// Return total number of conifers produced
+	return $totalConifers;
+}
+
+// Function to calculate the total number of wood produced by a given tree
+function calculateTreeWood($treeWood) {
+	// Calculate the number of wood produced by the tree
+	$totalWood = $treeWood * 19;
+	
+	// Return total number of wood produced
+	return $totalWood;
+}
+
+// Function to calculate the total number of roots of a given tree
+function calculateTreeRoots($treeRoots) {
+	// Calculate the number of roots of the tree
+	$totalRoots = $treeRoots * 20;
+	
+	// Return total number of roots
+	return $totalRoots;
+}
+?>
